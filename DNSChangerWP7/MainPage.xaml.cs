83cc4d679f2886f90938edc58108e562e5f08b6c
Jesse <jessenic@digiex.net>
2012-12-15 17:17:51 +0200

    Adapter list now working

--- DNSChangerWP7/MainPage.xaml.cs
+++ DNSChangerWP7/MainPage.xaml.cs
@@ -10,6 +10,10 @@ using System.Windows.Media;
 using System.Windows.Media.Animation;
 using System.Windows.Shapes;
 using Microsoft.Phone.Controls;
+using WP7RootToolsSDK;
+#if DEBUG
+using System.Diagnostics;
+#endif
 
 namespace DNSChangerWP7
 {
@@ -19,6 +23,60 @@ namespace DNSChangerWP7
         public MainPage()
         {
             InitializeComponent();
+            if (!WP7RootToolsSDK.Environment.HasRootAccess())
+            {
+                MessageBox.Show("This app requires Root access");
+            }
+        }
+
+        private void PhoneApplicationPage_Loaded(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                RegistryKey commKey = Registry.GetKey(RegistryHyve.LocalMachine, "Comm");
+                foreach (RegistryItem subitem in commKey.GetSubItems())
+                {
+                    if (subitem is RegistryKey)
+                    {
+                        RegistryKey subkey = (RegistryKey)subitem;
+#if DEBUG
+                        Debug.WriteLine(subkey.Name);
+#endif
+                        //TODO: Cleaner way to check if a key exists
+                        try
+                        {
+                            RegistryKey tcpipkey = subkey.GetSubKey("Parms\\Tcpip");
+                            if (tcpipkey.GetValueCount() > 0)
+                            {
+                                string DNS = "none";
+                                try
+                                {
+                                    DNS = String.Join(", ", Registry.GetMultiStringValue(tcpipkey.Root, tcpipkey.Path, "DNS"));
+                                }
+                                catch (Exception)
+                                {
+                                    try
+                                    {
+                                        DNS = "DHCP: " + String.Join(", ", Registry.GetMultiStringValue(tcpipkey.Root, tcpipkey.Path, "DhcpDNS"));
+                                    }
+                                    catch (Exception) { }
+                                }
+                                lstAdapters.Items.Add(new lstAdapterItem() { Name = subkey.Name, Description = DNS });
+                            }
+                        }
+                        catch (Exception) { }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.StackTrace, ex.Message, MessageBoxButton.OK);
+            }
+        }
+        public class lstAdapterItem
+        {
+            public string Name { get; set; }
+            public string Description { get; set; }
         }
     }
 }
